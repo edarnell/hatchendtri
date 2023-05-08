6261402c5f4f9783f8995ee5512697334228a50f
Ed Darnell <epdarnell@gmail.com>
2023-05-08 10:29:54 +0100

    live

---
+++ deployNode.bat
@@ -0,0 +1,3 @@
+scp -i ../.ssh/epdarnell.pem node/hatchend.mjs ubuntu@ec2.freemaths.uk:/var/www/hatchendtri/node
+scp -i ../.ssh/epdarnell.pem node/zip.mjs ubuntu@ec2.freemaths.uk:/var/www/hatchendtri/node
+scp -i ../.ssh/epdarnell.pem node/mail.mjs ubuntu@ec2.freemaths.uk:/var/www/hatchendtri/node
