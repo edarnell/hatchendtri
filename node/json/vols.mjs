e260663f324a957a584b258a4a93c9fb922927a8
Ed Darnell <epdarnell@gmail.com>
2024-05-08 06:04:14 +0100

    tidy

--- node/json/vols.mjs
+++ node/json/vols.mjs
@@ -2,14 +2,14 @@ import { fz } from '../zip.mjs'
 import fs from 'fs'
 
 function emails() {
-    const vs = fz(`gz/vs.gz`), vr = fz(`gz/vr.gz`), es = {}
+    const vs = fz(`live/vs.gz`), vr = fz(`live/vr.gz`), es = {}
     for (const [id, vi] of Object.entries(vr)) {
         if (vi.adult || vi.junior) {
             const v = vs[id], e = v.email.toLowerCase()
             if (e) es[e] = e;
         }
     }
-    const s = Object.values(es).sort().join(';')
+    const s = Object.values(es).sort().join('; ')
     fs.writeFileSync('emails_bcc.txt', s)
 }
 emails()
